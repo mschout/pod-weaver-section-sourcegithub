7ed381d0e718f44fa37b9fbfba22f2677e771fe1
Michael Schout <mschout@gkg.net>
2011-02-01 10:48:49 -0600

    update to work with Dist::Zilla 4.x, rework text a bit

--- lib/Pod/Weaver/Section/SourceGitHub.pm
+++ lib/Pod/Weaver/Section/SourceGitHub.pm
@@ -8,6 +8,22 @@ with 'Pod::Weaver::Role::Section';
 
 use Moose::Autobox;
 
+has zilla => (
+    is  => 'rw',
+    isa => 'Dist::Zilla');
+
+has repo_data => (
+    is         => 'ro',
+    lazy_build => 1);
+
+has repo_git => (
+    is         => 'ro',
+    lazy_build => 1);
+
+has repo_web => (
+    is         => 'ro',
+    lazy_build => 1);
+
 =method weave_section
 
 adds the C<SOURCE> section.
@@ -18,35 +34,25 @@ sub weave_section {
     my ($self, $document, $input) = @_;
 
     my $zilla = $input->{zilla} or return;
+    $self->zilla($zilla);
 
     my $meta = eval { $zilla->distmeta }
         or die "no distmeta data present";
 
     # pull repo out of distmeta resources.
-    my $repo = $meta->{resources}{repository} or return;
+    my $repo = $meta->{resources}{repository}{url} or return;
 
     return unless $repo =~ /github\.com/;
 
     my $clonerepo = $repo;
 
     # fix up clone repo url
-    if ($clonerepo =~ m#^http://#) {
-        $clonerepo =~ s#^http://#git://#;
-    }
-    if ($clonerepo !~ /\.git$/) {
-        $clonerepo .= '.git';
-    }
+    my $repo_web = $self->repo_web;
+    my $repo_git = $self->repo_git;
 
     my $text =
-        "You can contribute or fork this project via github:\n".
-        "\n".
-        "$repo\n";
-
-    # if repo differs from the clone repo, add clone command.
-    if ($clonerepo ne $repo) {
-        $text .= "\n".
-                 " git clone $clonerepo";
-    }
+        "The development version is on github at L<".$self->repo_web.">\n".
+        "and may be cloned from L<".$self->repo_git.">\n";
 
     $document->children->push(
         Pod::Elemental::Element::Nested->new({
@@ -57,6 +63,34 @@ sub weave_section {
             ],
         }),
     );
+}
+
+sub _build_repo_data {
+    my $self = shift;
+
+    my $url = $self->zilla->distmeta->{resources}{repository}{url}
+        or die "No repository URL found in distmeta";
+
+    if ($url =~ /github\.com/i) {
+        $url =~ s{^(?:http|git):/*}{}i;
+        $url =~ s{^git\@github.com:/*}{github.com/}i;
+        $url =~ s/\.git$//i;
+
+        my $repo_web = "http://$url";
+        my $repo_git = "git://$url.git";
+
+        return [ $repo_git, $repo_web ];
+    }
+
+    return [];
+}
+
+sub _build_repo_git {
+    shift->repo_data->[0];
+}
+
+sub _build_repo_web {
+    shift->repo_data->[1];
 }
 
 no Moose;
